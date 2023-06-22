51c2bed95f8bfd218e5ad6df453d6d597c1118d8
Smarandii <olegsmarandi@gmail.com>
2023-06-22 17:43:16 +0300

    Stage 1. | Initial commit

---
+++ fill_pdf_from_json/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.IO;
+using iText.Kernel.Pdf;
+using iText.Forms;
+using iText.Forms.Xfa;
+using iText.Forms.Fields;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using iText.Layout.Properties;
+
+public class Item
+{
+    public string S1_MiddleName { get; set; }
+    public string S1_GivenName { get; set; }
+    public string S1_FamilyName { get; set; }
+    public string S1_DateOfBirth { get; set; }
+    public string S2B_State { get; set; }
+    public string S2B_ZipCode { get; set; }
+    public bool S2B_Unit0 { get; set; }
+    public bool S2B_Unit1 { get; set; }
+    public bool S2B_Unit2 { get; set; }
+    public string S2B_AptSteFlrNumber { get; set; }
+    public string S2B_StreetNumberName { get; set; }
+    public string S2B_CityOrTown { get; set; }
+    public string S2C_State { get; set; }
+    public string S2C_ZipCode { get; set; }
+    public bool S2C_Unit0 { get; set; }
+    public bool S2C_Unit1 { get; set; }
+    public bool S2C_Unit2 { get; set; }
+    public string S2C_AptSteFlrNumber { get; set; }
+    public string S2C_StreetNumberName { get; set; }
+    public string S2C_CityOrTown { get; set; }
+    public string AlienNumber { get; set; }
+    public string S3_SignatureApplicant { get; set; }
+    public string S3_DateofSignature { get; set; }
+    public string S2A_State { get; set; }
+    public string S2A_ZipCode { get; set; }
+    public bool S2A_Unit0 { get; set; }
+    public bool S2A_Unit1 { get; set; }
+    public bool S2A_Unit2 { get; set; }
+    public string S2A_AptSteFlrNumber { get; set; }
+    public string S2A_StreetNumberName { get; set; }
+    public string S2A_CityOrTown { get; set; }
+}
+
+
+class FlattenXFA
+{
+    private const string XFAFilePathInput = @"E:\Projects\Python Projects\FillPdfForms\fill_pdf_from_json\ar-11-unlocked.pdf";
+    private const string XFAFilePathOutput = @"E:\Projects\Python Projects\FillPdfForms\fill_pdf_from_json\ar-11-unlocked-filled.pdf";
+    private const string JsonFileInput = @"E:\Projects\Python Projects\FillPdfForms\fill_pdf_from_json\test-data.json";
+
+    static void Main(string[] args)
+    {
+        FillPdfFieldsWithJsonValues();
+    }
+
+    public static string generateJsonKeyForPdfFieldKey(string initialFieldKey)
+    {
+        Console.WriteLine(initialFieldKey);
+        string jsonKey = initialFieldKey.Replace("form1[0].#subform[0]", "");
+        jsonKey = jsonKey.Replace("[0]", "");
+        jsonKey = jsonKey.Replace("[1]", "_1");
+        jsonKey = jsonKey.Replace("[2]", "_2");
+        jsonKey = jsonKey.Replace(".", "");
+        jsonKey = jsonKey.Replace("__", "_");
+
+        if (jsonKey.Length > 0 || jsonKey != "form" || jsonKey != "form1")
+            return jsonKey;
+        else
+            return "";
+    }
+
+    public static JObject getInputJsonValues()
+    {
+        string jsonString = File.ReadAllText(JsonFileInput);
+        JObject inputJsonValues = JObject.Parse(jsonString);
+        return inputJsonValues;
+    }
+
+    public static int getIndexOfSelectedState(KeyValuePair<string, PdfFormField> pdfField, JObject inputJsonValues, string jsonKey)
+    {
+        PdfArray options = pdfField.Value.GetOptions();
+        int cnt = 0;
+        foreach (PdfObject option in options)
+        {
+            if (option.ToString() == Convert.ToString(inputJsonValues[jsonKey]))
+            {
+                break;
+            }
+            cnt++;
+        }
+        return cnt;
+    }
+
+    public static void FillPdfFieldsWithJsonValues()
+    {
+        using (PdfReader reader = new PdfReader(XFAFilePathInput))
+        using (PdfWriter writer = new PdfWriter(XFAFilePathOutput))
+        using (PdfDocument pdfDoc = new PdfDocument(reader, writer))
+        {
+            PdfAcroForm form = PdfAcroForm.GetAcroForm(pdfDoc, true);
+            IDictionary<String, PdfFormField> fieldsDictionary = form.GetAllFormFields();
+
+            JObject inputJsonValues = getInputJsonValues();
+            foreach (KeyValuePair<string, PdfFormField> pdfField in fieldsDictionary)
+            {
+
+                string jsonKey = generateJsonKeyForPdfFieldKey(pdfField.Key);
+                if (jsonKey != "")
+                {
+                    Console.WriteLine(jsonKey);
+                    if (jsonKey == "S2B_Unit" || jsonKey == "S2B_Unit_1" || jsonKey == "S2B_Unit_2")
+                    {
+                        pdfField.Value.SetValue(Convert.ToString(inputJsonValues[jsonKey]));
+                    }
+                    if (jsonKey == "S2B_State" || jsonKey == "S2C_State" || jsonKey == "S2A_State")
+                    {
+                        PdfChoiceFormField stateChoiceField = (PdfChoiceFormField)pdfField.Value;
+                        int indexOfSelectedState = getIndexOfSelectedState(pdfField, inputJsonValues, jsonKey);
+                        int[] ListSelected = { indexOfSelectedState };
+                        stateChoiceField.SetListSelected(ListSelected);
+                    }
+                    else
+                    {
+
+                        pdfField.Value.SetValue(Convert.ToString(inputJsonValues[jsonKey]));
+                    }
+                    //Console.WriteLine("Field: " + jsonKey);
+                }
+            }
+            pdfDoc.Close();
+        }
+
+
+    }
+}
