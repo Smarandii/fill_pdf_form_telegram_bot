97ba961a005a6c8dacc481a6d89b8f0a5248d2ab
Смаранди Олег <Oleg Smarandi>
2023-11-06 13:15:40 +0300

    Deploy preparations

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -1,13 +1,31 @@
-from aiogram.dispatcher import FSMContext
+import os
+import json
+import logging
 import aiofiles
-from telegram_bot import bot, dp, types, filters, AvailableFormsKeyboard
 from phrases import START_PHRASE
+from aiogram.dispatcher import FSMContext
+from aiogram.utils.executor import start_webhook
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from telegram_bot import bot, dp, types, filters, AvailableFormsKeyboard
 from telegram_bot.form_ar_11.form_ar_11_state_group import Form_AR_11
 from telegram_bot.form_i_131.form_i_131_state_group import FormI131
 from telegram_bot.form_i_485.form_i_485_state_group import FormI485
 from telegram_bot.form_i_589.form_i_589_state_group import Form_I_589
 from telegram_bot.form_i_765.form_i_765_state_group import FormI765
-import json
+from flask import Flask, request
+
+
+WEBHOOK_HOST = 'https://78d3-194-5-212-109.ngrok-free.app'
+WEBHOOK_PATH = f'/webhook/{os.getenv("API_TOKEN")}'
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+WEBAPP_HOST = 'localhost'  # or 'localhost'
+WEBAPP_PORT = os.getenv('PORT', default=5000)
+
+logging.basicConfig(level=logging.INFO)
+dp.middleware.setup(LoggingMiddleware())
+
+app = Flask(__name__)
 
 
 @dp.message_handler(filters.Command("start"), state="*")
@@ -56,19 +74,30 @@ from form_i_765 import form_i_765_handlers
 from form_i_485 import form_i_485_handlers
 from form_i_131 import form_i_131_handlers
 
-async def on_startup(dp):
+
+@app.route(WEBHOOK_PATH, methods=['POST'])
+async def webhook(request):
+    update = types.Update(**request.json)
+    await dp.process_update(update)
+    return '', 200
+
+
+async def on_startup(dispatcher):
     await bot.send_message(chat_id=231584958, text='Bot has been started')
+    await bot.set_webhook(WEBHOOK_URL)
 
 
-async def on_shutdown(dp):
+async def on_shutdown(dispatcher):
     await bot.send_message(chat_id=231584958, text='Bot has been stopped')
+    await bot.delete_webhook()
+
 
 if __name__ == '__main__':
-    from aiogram import executor
-    while True:
-        try:
-            executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
-        except Exception as e:
-            bot.send_message(chat_id=231584958, text=f'{e.args[0]} | {e.args}')
-
-
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
