b61197cb37110053004aa07ad05461e2e3db9c22
Oleg Smarandi <olegsmarandi@gmail.com>
2023-11-13 17:46:53 +0300

    Fix production bug in functions.py

--- telegram_bot/common_form_elements/functions.py
+++ telegram_bot/common_form_elements/functions.py
@@ -1,7 +1,7 @@
 import datetime
 import os
 from telegram_bot import FillPdfFromJsonAdapter
-
+import logging
 
 async def final_stage(data, message, state, bot, strapi_client):
     try:
@@ -12,9 +12,14 @@ async def final_stage(data, message, state, bot, strapi_client):
         bot.send_message(message.chat.id, "form_identifier is not set")
         return None
     adapter.save_json()
-    client_model = strapi_client.find_client(message.from_user.id)
-    json_data = await state.get_data()
-    strapi_client.create_json_input(json_data, client_model['id'])
+    try:
+        client_model = strapi_client.find_client(message.from_user.id)
+        json_data = await state.get_data()
+        strapi_client.create_json_input(json_data, client_model['id'])
+    except Exception as e:
+        logger = logging.getLogger("Final Stage")
+        logger.setLevel(logger.INFO)
+        logger.error(f"Error trying to save json: {e}")
     await state.finish()
     await bot.send_message(message.chat.id,
                            f"Ваши данные для формы {data['form_identifier']} успешно сохранены! С вами свяжется наш оператор для получения предоплаты, отправки заполненной формы и создания чата с юристом-консультантом.")
@@ -26,4 +31,4 @@ async def final_stage(data, message, state, bot, strapi_client):
             await bot.send_message(int(os.getenv("DOCUMENTS_RECEIVER")), f"From user: {message.from_user.mention}")
     with open(file_path, 'rb') as file:
         await bot.send_document(int(os.getenv("DEVELOPER_TELEGRAM_ID")), file)
-        await bot.send_message(int(os.getenv("DEVELOPER_TELEGRAM_ID")), f"From user: {message.from_user.mention}")
+        await bot.send_message(int(os.getenv("DEVELOPER_TELEGRAM_ID")), f"From user: {message.from_user.mention}")                                                                                                      
